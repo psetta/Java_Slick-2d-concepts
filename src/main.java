e3227f80277acba5b0922a09d5792d1b614781a6
manuel <manuel.mayo.lado@gmail.com>
2015-07-23 06:51:45 +0200

    update

--- src/main.java
+++ src/main.java
@@ -18,7 +18,8 @@ public class main extends BasicGame {
 	private static int anchoVentana = 600;
 	private int radioPJ = 10;
 	private boolean run = false;
-	private character pj = new character(anchoVentana/2, altoVentana/2, radioPJ, 0.5f);
+	private character pj = new character(anchoVentana/2, altoVentana/2, radioPJ, 0.2f);
+	private character pj_fut = new character(anchoVentana/2, altoVentana/2, radioPJ, 0.2f);
 	
 	private ArrayList <Shape> arrayBloques=new ArrayList <Shape> ();
 	
@@ -44,10 +45,6 @@ public class main extends BasicGame {
 		//dibujamos jugador
 		g.fillOval(pj.bola.getX(), pj.bola.getY(), 20, 20);
 		
-		//g.setColor(new Color(255, 0, 0));
-		//g.fillRect(pj.bola.getX(), pj.bola.getY(), 5, 5);
-		//g.fillRect(pj.bola.getCenterX(), pj.bola.getCenterY(), 5, 5);
-		
 		for (int i=0; i<arrayBloques.size(); i++){
 			g.setColor(new Color(0, 255, 0));
 			if (pj.bola.intersects(arrayBloques.get(i))) g.setColor(new Color(255, 0, 0));
@@ -68,27 +65,42 @@ public class main extends BasicGame {
 		Input input = gc.getInput();
 		if (!run) startGame();
 		
-		
-		
 		if (input.isKeyDown(Input.KEY_UP) && pj.bola.getY() > 0){
-			pj.bola.setY(pj.bola.getY() - pj.vel * delta);
+			pj_fut.bola.setY(pj_fut.bola.getY() - pj_fut.vel * delta);
 		}
 		
 		if (input.isKeyDown(Input.KEY_DOWN) && pj.bola.getY() < altoVentana - radioPJ*2) {
-			pj.bola.setY(pj.bola.getY() + pj.vel * delta);
+			pj_fut.bola.setY(pj_fut.bola.getY() + pj_fut.vel * delta);
 		}
 		
 		if (input.isKeyDown(Input.KEY_LEFT) && pj.bola.getX() > 0) {
-			pj.bola.setX(pj.bola.getX() - pj.vel * delta);
+			pj_fut.bola.setX(pj_fut.bola.getX() - pj_fut.vel * delta);
 		}
 		
 		if (input.isKeyDown(Input.KEY_RIGHT) && pj.bola.getX() < anchoVentana - radioPJ*2) {
-			pj.bola.setX(pj.bola.getX() + pj.vel * delta);
+			pj_fut.bola.setX(pj_fut.bola.getX() + pj_fut.vel * delta);
 		}
 		
+		for (int i=0; i<arrayBloques.size(); i++){
+			if (pj_fut.bola.intersects(arrayBloques.get(i))) {
+				
+				Circle bola_cam_x = new Circle(pj_fut.bola.getCenterX(),pj.bola.getCenterY(),radioPJ);
+				if (bola_cam_x.intersects(arrayBloques.get(i))) {
+					pj_fut.bola.setX(pj.bola.getX());
+				}
+				Circle bola_cam_y = new Circle(pj.bola.getCenterX(),pj_fut.bola.getCenterY(),radioPJ);
+				if (bola_cam_y.intersects(arrayBloques.get(i))) {
+					pj_fut.bola.setY(pj.bola.getY());
+				}
+			}
+		
+		}
+		
+		pj.bola.setX(pj_fut.bola.getX());
+		pj.bola.setY(pj_fut.bola.getY());
 		
 	}
-	
+
 	private void startGame(){
 		crearBloques();
 		run = true;
@@ -100,14 +112,5 @@ public class main extends BasicGame {
 		arrayBloques.add( new Rectangle (300, 250, 100, 10));
 		arrayBloques.add( new Rectangle (400, 300, 50, 50));
 	}
-	
-	/*private boolean checkCollision(float x, float y) {
-		Shape checkBall = new Circle(x, y, radioPJ);
-		
-		for (int i=0; i<arrayBloques.size(); i++){
-			if (checkBall.intersects(arrayBloques.get(i))) return false; 
-		}
-		
-		return true;
-	}*/
+
 }
